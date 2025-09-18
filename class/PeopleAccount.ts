9beeacf4128d47e3d968d19a5f67d459d5a8d6a5
Giovanni Donati <grdonati99@gmail.com>
2025-09-18 17:25:09 -0300

    feat: update methods and on PeopleAccount class

--- class/PeopleAccount.ts
+++ class/PeopleAccount.ts
@@ -1,10 +1,19 @@
 import { DioAccount } from "./DioAccount"
 
 export class PeopleAccount extends DioAccount {
-  doc_id: number
+  private doc_id: number
 
   constructor(doc_id: number, name: string, accountNumber: number){
     super(name, accountNumber)
     this.doc_id = doc_id
+  }
+
+  getDoc = (): number => {
+    return this.doc_id
+  }
+
+  setDoc = (doc_id: number): void => {
+    this.doc_id = doc_id
+    console.log('Nome alterado com sucesso!')
   }
 }
