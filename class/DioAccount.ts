60f33a1786a23c8a6a8c594f80c9d4121386bdba
Giovanni Donati <grdonati99@gmail.com>
2025-09-19 10:31:08 -0300

    refact: reestructure validate on methods

--- class/DioAccount.ts
+++ class/DioAccount.ts
@@ -9,44 +9,49 @@ export abstract class DioAccount {
     this.accountNumber = accountNumber
   }
 
-  getName = (): string => {
-    return this.name
-  }
+  getName = (): string => { return this.name }
+  getBalance = (): number => { return this.balance }
 
-  deposit = (value: number): void => {
-    if (this.validateStatus()){
-      if(value > 0){
-      this.balance += value
-      console.log(`Depósito de R$ ${value} efetuado!`);
-      }
-    }
-  }
-  
-  withdraw = (value: number): void => {
-    if (this.validateStatus()){
-      if (value < this.balance || value < 0){
-        this.balance -= value
-        console.log(`Saque de R$ ${value} realizado!`);
-        } else {
-          console.log("Saldo insuficiente");
-          // throw new Error("Saldo insuficiente!")
-        }
-    }
-  }
-
-  getBalance = (): number => {
-    console.log(`SALDO ATUAL: R$ ${this.balance}`)
-    return this.balance
-  }
+  private setBalance = (value: number): void => {
+    this.balance = value;
+  };
 
   setStatus = (status: boolean): void => {
     this.status = status
   }
 
-  validateStatus = (): boolean => {
-    if (this.status) {
-      return this.status
+  deposit(value: number): void {
+    console.log(`Operação para depositar R$ ${value}`)
+    if (!this.validateStatus()){ return; }
+    if(value < 0){ return; }
+
+    this.setBalance(this.balance + value)
+    console.log(`Depósito de R$ ${value} efetuado!`);
+  }
+  
+  withdraw(value: number): void {
+    console.log(`Operação para sacar R$ ${value}`)
+    if (!this.validateStatus()){ return; }
+    if (!this.validateWithdraw(value)){ return; }
+
+    if (value <= 0){
+      console.log("Valor deve ser positivo!");
+      return;
     }
+
+    this.setBalance(this.balance - value)
+    console.log(`Saque de R$ ${value} realizado!`);
+    // throw new Error("Saldo insuficiente!")
+    }
+
+  validateWithdraw = (value: number): boolean => {
+    if (value <= this.getBalance()) { return true; }
+    console.log("Saldo insuficiente");
+    return false;
+   }
+
+  validateStatus = (): boolean => {
+    if (this.status) { return this.status }
     // throw new Error('Conta inativa')
     console.log('Conta inativa')
     return(false)
