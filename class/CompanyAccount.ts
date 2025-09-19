c98418b880c1d6d76b26c71ba5ace5cadc4c667a
Giovanni Donati <grdonati99@gmail.com>
2025-09-19 08:01:19 -0300

    feat: Implement method to getLoan on CompanyAccount

--- class/CompanyAccount.ts
+++ class/CompanyAccount.ts
@@ -6,7 +6,25 @@ export class CompanyAccount extends DioAccount {
     super(name, accountNumber)
   }
 
-  getLoan = (): void => {
-    console.log('Voce pegou um empréstimo')
+  getLoan = (value: number, Account: DioAccount): void => {
+    if (Account.validateStatus()){
+      if (value > 0 && this.validateLoan(value)){
+        this.withdraw(value)
+        Account.deposit(value)
+        console.log(`Empréstimo de R$ ${value} para ${Account.getName()} realizado com sucesso!`);
+      } else {
+        // throw new Error("Valor do empréstimo deve ser positivo")
+        console.log("Valor do empréstimo deve ser positivo!");
+      }
+    }
+  }
+
+  validateLoan = (value: number): boolean => {
+    if (this.getBalance() > value) {
+      return true
+    } else {
+      console.log("Saldo indisponível para emprestimo!")
+      return false
+    }
   }
 }
